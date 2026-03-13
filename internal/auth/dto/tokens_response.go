e1741e4485403e96d9fb8b77093e6561df02b7de
Milana <bigaeva03@inbox.ru>
2026-03-13 15:58:22 +0300

    feat(dto): добавляю DTO для сброса пароля и обновляю TokensResponse

--- internal/auth/dto/tokens_response.go
+++ internal/auth/dto/tokens_response.go
@@ -4,6 +4,8 @@ import "time"
 
 // TokensResponse содержит пару токенов для клиента
 type TokensResponse struct {
+	AccessToken      string        `json:"-"` // Не показывать в JSON ответе
+	RefreshToken     string        `json:"-"` // Не показывать в JSON ответе
 	AccessExpiresIn  time.Duration `json:"accessExpiresIn"`
 	RefreshExpiresIn time.Duration `json:"refreshExpiresIn"`
 }
